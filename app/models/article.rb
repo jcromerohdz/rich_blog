2754ddae61805dc0f361875fad52cab858528e62
J. Christian Romero <jcromerohdz@gmail.com>
2019-12-08 09:40:21 -0800

    Implementing Destroy functionallity for articles associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,6 +10,8 @@ class Article < ApplicationRecord
 		# Change the elements to an array
     # categories_array = category_elements.split(",")
 		# Loop the array
+		return has_categories.destroy_all if category_elements.nil? || category_elements.empty?
+		has_categories.where.not(category_id: category_elements).destroy_all 
 		category_elements.each do |category_id|
 		  # Create HasCategory
 			HasCategory.find_or_create_by(article: self, category_id: category_id)
